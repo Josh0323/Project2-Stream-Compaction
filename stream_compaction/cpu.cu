a640132f04a8014cf25971ad78c9065888880f68
Josh0323 <joshkimlab@gmail.com>
2026-09-14 02:09:07 +0000

    CPU scan and compaction

--- stream_compaction/cpu.cu
+++ stream_compaction/cpu.cu
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <vector>
 #include "cpu.h"
 
 #include "common.h"
@@ -13,13 +14,27 @@ namespace StreamCompaction {
         }
 
         /**
+         * Untimed exclusive prefix sum. compactWithScan needs a scan too, but it can't
+         * call scan() because starting the CPU timer while it's running throws.
+         * Reading idata[i] before writing odata[i] keeps this safe when odata == idata.
+         */
+        static void prefixSum(int n, int *odata, const int *idata) {
+            int sum = 0;
+            for (int i = 0; i < n; i++) {
+                int value = idata[i];
+                odata[i] = sum;
+                sum += value;
+            }
+        }
+
+        /**
          * CPU scan (prefix sum).
          * For performance analysis, this is supposed to be a simple for loop.
          * (Optional) For better understanding before starting moving to GPU, you can simulate your GPU scan in this function first.
          */
         void scan(int n, int *odata, const int *idata) {
             timer().startCpuTimer();
-            // TODO
+            prefixSum(n, odata, idata);
             timer().endCpuTimer();
         }
 
@@ -30,9 +45,14 @@ namespace StreamCompaction {
          */
         int compactWithoutScan(int n, int *odata, const int *idata) {
             timer().startCpuTimer();
-            // TODO
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                if (idata[i] != 0) {
+                    odata[count++] = idata[i];
+                }
+            }
             timer().endCpuTimer();
-            return -1;
+            return count;
         }
 
         /**
@@ -41,10 +61,28 @@ namespace StreamCompaction {
          * @returns the number of elements remaining after compaction.
          */
         int compactWithScan(int n, int *odata, const int *idata) {
+            // Allocate outside the timed region, same rule as cudaMalloc for the GPU versions.
+            std::vector<int> bools(n > 0 ? n : 0);
+            std::vector<int> indices(n > 0 ? n : 0);
+
             timer().startCpuTimer();
-            // TODO
+            // Map: 1 for elements we keep, 0 for elements we drop.
+            for (int i = 0; i < n; i++) {
+                bools[i] = idata[i] != 0 ? 1 : 0;
+            }
+
+            // Scan: indices[i] is where idata[i] lands in the output, if kept.
+            prefixSum(n, indices.data(), bools.data());
+
+            // Scatter.
+            for (int i = 0; i < n; i++) {
+                if (bools[i]) {
+                    odata[indices[i]] = idata[i];
+                }
+            }
+            int count = n > 0 ? indices[n - 1] + bools[n - 1] : 0;
             timer().endCpuTimer();
-            return -1;
+            return count;
         }
     }
 }
